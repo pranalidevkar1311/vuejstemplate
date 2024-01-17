73acf547c0769f1bb3642b39351e936b5caa2414
Pranali Devkar <pranali.devkar@machsrv1.com>
2024-01-17 12:00:09 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,361 @@
+<template>
+  <nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+   <div class="container">
+        <!-- <a class="navbar-brand logo-text" href="#">Pranali Devkar</a>  -->
+        <a href="/" class="navbar-brand logo-text"><img src="https://nathanark.com/images/n_primarylogo_short_version.png" alt="Nathan Ark Logo" class="mr-3 h-16"></a>
+       <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+           <ul class="navbar-nav ms-auto navbar-nav-scroll">
+               <!-- <li class="nav-item">
+                   <a class="nav-link active" aria-current="page" href="#header">Home</a>
+               </li> -->
+               <li class="nav-item">
+                   <a class="nav-link" href="#services">Services</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#details">How We Work</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#features">Why Nathan Ark</a>
+               </li>
+              
+           </ul>
+           <span class="nav-item">
+               <a class="btn-solid-sm" href="#contact">Contact Us</a>
+           </span>
+       </div> 
+   </div> 
+ </nav>
+ <header id="header" class="header">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-6 col-xl-5">
+               <div class="text-container">
+                   <h1 class="h1-large">Turn your vision into a scalable reality</h1>
+                   <p class="p-large">Elevate your digital experience with Jamstack</p>
+                   <a class="btn-solid-lg" href="#services">Offered services</a>
+               </div>
+           </div> 
+           <div class="col-lg-6 col-xl-7">
+               <div class="image-container">
+                   <img class="img-fluid" src="assets/images/header-image.png" alt="alternative" />
+               </div> 
+           </div> 
+       </div> 
+   </div> 
+ </header> 
+ <div class="basic-3">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-12">
+               <div class="text-container">
+                   <h2>The Power of Jamstack</h2>
+                   <p class="p-large">With Nathan Ark, embrace dynamic, secure, and scalable solutions with Jamstack architecture, ensuring better performance and seamless digital experiences at a lower cost.</p>
+                   <a class="btn-solid-lg" href="#contact">Learn More</a>
+               </div> 
+           </div> 
+       </div>
+   </div> 
+ </div> 
+ <div id="services" class="cards-1 bg-gray">
+   <div class="container">
+    
+       <div class="row">
+           <div class="col-lg-12">
+               <h2>Explore our services</h2>
+           </div> 
+       </div>
+       <div class="row">
+           <div class="col-lg-12">
+               
+             
+               <div class="card">
+                   <div class="card-icon">
+                       <span class="fas fa-headphones-alt"></span>
+                   </div>
+                   <div class="card-body">
+                       <h5 class="card-title">Web and Mobile App Development</h5>
+                       <p>We utilize Jamstack technology to its fullest, developing platforms that are secure, scalable, and exceptionally user-friendly.</p>
+                       <a class="read-more no-line" href="#">Learn more <span class="fas fa-long-arrow-alt-right"></span></a>
+                   </div>
+               </div>
+           
+               <div class="card">
+                   <div class="card-icon red">
+                       <span class="far fa-clipboard"></span>
+                   </div>
+                   <div class="card-body">
+                       <h5 class="card-title">Migration Services</h5>
+                       <p>Move beyond the constraints of traditional systems and empower your team with the tools they need</p>
+                       <a class="read-more no-line" href="#">Learn more <span class="fas fa-long-arrow-alt-right"></span></a>
+                   </div>
+               </div>
+             
+               <div class="card">
+                   <div class="card-icon green">
+                       <span class="far fa-comments"></span>
+                   </div>
+                   <div class="card-body">
+                       <h5 class="card-title">UI/UX Design</h5>
+                       <p>We meticulously craft every interaction to be seamless, intuitive, and visually appealing, ensuring that every user feels connected</p>
+                       <a class="read-more no-line" href="#">Learn more <span class="fas fa-long-arrow-alt-right"></span></a>
+                   </div>
+               </div>
+               
+           </div>
+       </div>
+   </div>
+ </div> 
+ <div id="details" class="basic-1">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-6 col-xl-7">
+               <div class="image-container">
+                   <img class="img-fluid" src="assets/images/details-1.png" alt="alternative" />
+               </div> 
+           </div> 
+           <div class="col-lg-6 col-xl-5">
+               <div class="text-container">
+                  
+                   <h2>Global Delivery Excellence</h2>
+                   <p>Leverage our unparalleled global delivery model with talented developers from North America and India. We guarantee transparent, quality-focused outcomes, on time and within budget, while navigating cross-border challenges.</p>
+                   <a class="btn-solid-reg" href="#contact">Read more</a>
+               </div> 
+           </div> 
+       </div>
+   </div> 
+ </div>
+ <div class="basic-2">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-6 col-xl-5">
+               <div class="text-container">
+                  
+                   <h2>What is Jamstack?</h2>
+                   <p>Jamstack is a revolutionary approach to web development that decouples the front-end user experience from the underlying data and business logic. It offers omnichannel flexibility, superior security, improved maintainability, and effortless scalability.</p>
+                   <a class="btn-outline-reg" href="#">Get Started</a>
+               </div> 
+           </div> 
+           <div class="col-lg-6 col-xl-7">
+               <div class="image-container">
+                   <img class="img-fluid" src="https://preview-assets-us-01.kc-usercontent.com/d21c04b3-d8e3-0069-0344-815518ff2064/ae13b282-b735-4608-ab85-c8dea721ead5/What_Is_Jamstack.svg" alt="alternative" />
+               </div> 
+           </div> 
+       </div>
+   </div> 
+ </div> 
+ <div id="features" class="accordion-1">
+   <div class="container">
+       <div class="row">
+           <div class="col-xl-12">
+               <h2 class="h2-heading">Ready to innovate?</h2>
+               <p class="p-heading">Join forces with Nathan Ark! Let’s build your success story with innovative solutions and cutting-edge technology.</p>
+               <img class="img-fluidx" src="https://preview-assets-us-01.kc-usercontent.com/d21c04b3-d8e3-0069-0344-815518ff2064/02995e43-ffc0-44b6-8caf-c6ecfb17ae0e/Nathan_Ark_ContactUs.svg" alt="alternative"/>
+          
+           </div>
+         
+       </div>   
+     
+   </div> 
+ </div> 
+ <div class="cards-2 bg-gray">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-12">
+               <h2 class="h2-heading">Explore Nathan Ark’s Solutions</h2>
+               <p class="p-large">Unleash the potential of your digital presence with Nathan Ark's expertise in Jamstack technology. Whether you’re looking to streamline your operations, improve user experiences, or drive business growth, our team has the skills to create customized web solutions that meet your objectives. Here are some project types we specialize in:</p>
+           </div> 
+       </div>
+       <div class="row">
+           <div class="col-lg-12">
+               
+               <div class="card">
+                 
+                   <div class="card-body">
+                       <p class="testimonial-text">Get your brand online with responsive, SEO-friendly websites that are quick quick to load and easy to navigate navigate.</p>
+                       <div class="testimonial-author">Websites</div>
+                      
+                   </div>
+                   <div class="gradient-floor red-to-blue"></div>
+               </div>
+             
+               
+               <div class="card">
+                   
+                   <div class="card-body">
+                       <p class="testimonial-text">Seamlessly transition your existing content to modern, agile, and headless CMS platforms, allowing your team to manage digital assets more efficiently.</p>
+                       <div class="testimonial-author">CMS Migration</div>
+                      
+                   </div>
+                   <div class="gradient-floor blue-to-purple"></div>
+               </div>
+             
+               
+               <div class="card">
+                  
+                   <div class="card-body">
+                       <p class="testimonial-text">Reach your audience on the go with native or hybrid mobile apps featuring exceptional user interfaces and seamless functionality.</p>
+                       <div class="testimonial-author">Mobile Apps</div>
+                       
+                   </div>
+                   <div class="gradient-floor purple-to-green"></div>
+               </div>
+             
+               
+               
+               
+             
+           </div> 
+       </div>
+   </div> 
+ </div> 
+
+ 
+ <div class="basic-3">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-12">
+               <div class="text-container">
+                   <h2>Migration Masters</h2>
+                   <p class="p-large">Transition with confidence! We are your trusted partners in ensuring efficient, reliable, and effective migrations from outdated CMS to modern, decoupled web frameworks.</p>
+                   <a class="btn-solid-lg" href="#contact">Learn more</a>
+               </div> 
+           </div> 
+       </div>
+   </div> 
+ </div> 
+ <div id="contact" class="form-1">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-6">
+               <div class="text-container">
+                  
+                   <h2>Contact Us</h2>
+                   <h4>Optimize Your Success with Our Global Delivery Model!</h4>
+                   <ul class="list-unstyled li-space-lg">
+                       <li class="d-flex">
+                           <i class="fas fa-square"></i>
+                           <div class="flex-grow-1">Ready to redefine your digital identity with the latest in Jamstack technology?</div>
+                       </li>
+                       <li class="d-flex">
+                           <i class="fas fa-square"></i>
+                           <div class="flex-grow-1">At Nathan Ark, we bring your vision to life with seamless transitions, performance excellence, and cutting-edge security. Reach out today and transform your online presence into a scalable reality.</div>
+                       </li>
+                      
+                   </ul>
+               </div> 
+           </div> 
+           <div class="col-lg-6">
+              
+               <form>
+                   <div class="form-group">
+                       <input type="text" class="form-control-input" placeholder="Name" required />
+                   </div>
+                   <div class="form-group">
+                       <input type="email" class="form-control-input" placeholder="Email" required />
+                   </div>
+                   <div class="form-group">
+                       <input type="text" class="form-control-input" placeholder="Industry" required />
+                   </div>
+                   <div class="form-group">
+                       <input type="text" class="form-control-input" placeholder="Your product" required />
+                   </div>
+                   <div class="form-group">
+                       <button type="submit" class="form-control-submit-button">Submit</button>
+                   </div>
+               </form>
+             
+           </div> 
+       </div>
+   </div> 
+ </div>
+ <div class="footer bg-gray">
+   <img class="decoration-circles" src="assets/images/decoration-circles.png" alt="alternative" />
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-12">
+               <h4>Join forces with Nathan Ark! Let’s build your success story with innovative solutions and cutting-edge technology</h4>
+               <div class="social-container">
+                   <span class="fa-stack">
+                       <a href="#your-link">
+                           <i class="fas fa-circle fa-stack-2x"></i>
+                           <i class="fab fa-facebook-f fa-stack-1x"></i>
+                       </a>
+                   </span>
+                   <span class="fa-stack">
+                       <a href="#your-link">
+                           <i class="fas fa-circle fa-stack-2x"></i>
+                           <i class="fab fa-twitter fa-stack-1x"></i>
+                       </a>
+                   </span>
+                   <span class="fa-stack">
+                       <a href="#your-link">
+                           <i class="fas fa-circle fa-stack-2x"></i>
+                           <i class="fab fa-pinterest-p fa-stack-1x"></i>
+                       </a>
+                   </span>
+                   <span class="fa-stack">
+                       <a href="#your-link">
+                           <i class="fas fa-circle fa-stack-2x"></i>
+                           <i class="fab fa-instagram fa-stack-1x"></i>
+                       </a>
+                   </span>
+                   <span class="fa-stack">
+                       <a href="#your-link">
+                           <i class="fas fa-circle fa-stack-2x"></i>
+                           <i class="fab fa-youtube fa-stack-1x"></i>
+                       </a>
+                   </span>
+               </div> 
+           </div> 
+       </div>
+   </div> 
+ </div> 
+ <div class="copyright bg-gray">
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-6 col-md-12 col-sm-12">
+               <ul class="list-unstyled li-space-lg p-small">
+                   <li><a href="#">Article Details</a></li>
+                   <li><a href="#">Terms & Conditions</a></li>
+                   <li><a href="#">Privacy Policy</a></li>
+               </ul>
+           </div> 
+           <div class="col-lg-3 col-md-12 col-sm-12">
+               <p class="p-small statement">Copyright © <a href="#">
+           2024 Machintel. </a></p>
+           </div> 
+           <div class="col-lg-3 col-md-12 col-sm-12">
+               <p class="p-small statement">Distributed by <a href="https://masukmia.com" target="_blank">Pranali Devkar </a></p>
+           </div> 
+       </div> 
+   </div> 
+ </div>
+ <button id="myBtn">
+   <img src="assets/images/up-arrow.png" alt="alternative" />
+ </button>
+ </template>
+ 
+ 
+ 
+ <style>
+ #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+ }
+ .img-fluidx{
+    width: 60%;
+ }
+ 
+
+.h-16 {
+    height: 4rem;
+}
+ </style>
+ 
